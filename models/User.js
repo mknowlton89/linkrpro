c794929d0dbd377458d6f542bbee9f92cd89d365
Michael Knowlton <mknowlton89@gmail.com>
2021-10-02 16:12:59 -0400

    Got bcrypt package working upon signup.

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-// const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs")
 
 const UserSchema = new mongoose.Schema(
   {
@@ -40,6 +40,31 @@ const UserSchema = new mongoose.Schema(
     //   type: Number,
     //   default: 30
     // }
+  });
+
+  UserSchema.pre("save", function (next) {
+    const user = this
+
+    console.log('save was called')
+
+    if (this.isModified("password") || this.isNew) {
+      bcrypt.genSalt(10, function (saltError, salt) {
+        if (saltError) {
+          return next(saltError)
+        } else {
+          bcrypt.hash(user.password, salt, function (hashError, hash) {
+            if (hashError) {
+              return next(hashError)
+            }
+
+            user.password = hash
+            next()
+          })
+        }
+      })
+    } else {
+      return next()
+    }
   });
 
   export default mongoose.models.User || mongoose.model('User', UserSchema)
