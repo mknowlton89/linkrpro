c2fc7630f86e6f68d8de5ffdf0b2d0866b89439c
Michael Knowlton <mknowlton89@gmail.com>
2021-12-01 15:02:01 -0500

    Got a basic react table set up and working with real data. Need to work on styling and then add features like sorting, pagination, etc

--- components/LinkHistory.jsx
+++ components/LinkHistory.jsx
@@ -1,35 +1,100 @@
-import React, { useEffect, useState } from 'react'
-import { StyledLoading } from '../styles/StyledComponents';
-import LoginRequired from '../components/LoginRequired'
-import API from '../utils/API'
-import { useContext } from 'react';
-import { UserContext } from '../context/UserContext';
+import React, { useMemo } from 'react'
 import { PageContentWrapper } from '../styles/StyledComponents';
+import { useTable } from 'react-table'
 
-const LinkHistory = () => {
-    const [linkHistory, setLinkHistory] = useState([]);
-    const { user, setUser } = useContext(UserContext);
+const LinkHistory = (props) => {
+    let data = null;
 
-    useEffect(() => {
-        if (user) {
-            API.getLinkHistoryById(user._id)
-                .then((res) => setLinkHistory(res.data))
-                .catch(err => console.log(err))
+    data = props.data.map(link => {
+        return {
+            createdOn: link.createdOn,
+            link: link.link,
+            user: link.user,
         }
-    }, [user])
+    })
 
-    console.log(linkHistory)
+    const columns = useMemo(
+        () => [
+          {
+            Header: 'Date',
+            accessor: 'createdOn', // accessor is the "key" in the data
+          },
+          {
+            Header: 'URL',
+            accessor: 'link',
+          },
+          {
+            Header: 'User',
+            accessor: 'user'
+          }
+        ],
+        []
+    )
+
+    const tableInstance = useTable({ columns, data });
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = tableInstance;
 
     return (
         <>
             <PageContentWrapper>
-                    <h1>Your Link History</h1>
-                    <div className="table-row">
-                        {linkHistory.map((link) => {
-                            return <p key={link._id}>{link.link}</p>
+                <h1>Your Link History</h1>
+                <div className="table-wrapper">
+                    <table {...getTableProps()}
+                        style={{
+                            border: 'solid 1px black',
+                            fontSize: '15px'
+                        }}>
+                    <thead>
+                        {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map(column => (
+                            <th
+                                {...column.getHeaderProps()}
+                                style={{
+                                borderBottom: 'solid 1px black',
+                                color: 'black',
+                                fontWeight: 'bold',
+                                height: '25px',
+                                fontSize: '17px'
+                                }}
+                            >
+                                {column.render('Header')}
+                            </th>
+                            ))}
+                        </tr>
+                        ))}
+                    </thead>
+                    <tbody {...getTableBodyProps()}>
+                        {rows.map(row => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps()}>
+                            {row.cells.map(cell => {
+                                return (
+                                <td
+                                    {...cell.getCellProps()}
+                                    style={{
+                                    padding: '10px',
+                                    border: 'solid 1px gray',
+                                    }}
+                                >
+                                    {cell.render('Cell')}
+                                </td>
+                                )
+                            })}
+                            </tr>
+                        )
                         })}
-                    </div>
-
+                    </tbody>
+                    </table>
+                </div>
             </PageContentWrapper>
 
             <style jsx>{`
@@ -37,6 +102,16 @@ const LinkHistory = () => {
                     display: flex;
                     flex-direction: column;
                     padding: 20px;
+                }
+
+                .table-wrapper {
+                    width: 100%;
+                    display: flex;
+                    align-self: center;
+                }
+
+                table {
+                    width: 100%;
                 }
 
                 .table-title {
@@ -62,5 +137,7 @@ const LinkHistory = () => {
         </>
     )
 }
+
+// }
 
 export default LinkHistory
