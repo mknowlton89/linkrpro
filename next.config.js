980b54250c801e668614a14cc119411b937d879a
Michael Knowlton <mknowlton89@gmail.com>
2022-04-14 19:54:58 -0400

    Added MongoDB URI & DB env variables to config file.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     env: {
+        MONGODB_URI: 'mongodb+srv://mknowlton-admin:Michael2016!@cluster0.hfcgv.mongodb.net/utmGenerator?retryWrites=true&w=majority',
+        MONGODB_DB: 'utmGenerator',
         STRIPE_PUBLIC_KEY: "pk_live_51JbliZA2NnxEX769wLJJG4EafdJuEuUFQ1tC4vd5lQOatJpq0Nrae1uuSXN7YtvmY39qWTNKuYZJ9HQbQUwt8jte00cPyKTaMd",
         STRIPE_PUBLIC_TEST_KEY: "pk_test_51JbliZA2NnxEX7695GhMBYZIVuG1xDYLdrDTkSnwTmDIcbjp1llrwT4DqMK5KhFS5rVReKCM5P2oqq3kIlJStP4o004inxyI2N",
         STRIPE_SECRET_TEST_KEY: "sk_test_51JbliZA2NnxEX769QWNQEcOj9Ivq97Mxt1ZEkvrdaZPCrUChuUsLvnuMV2otI7N3RTaVA9CD5VSlrfHeqxLETxTd00RAgDKbM4"
