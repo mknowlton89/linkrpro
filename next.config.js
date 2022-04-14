f9a6114f33fc8501816a77de8cf17f74d662ab42
Michael Knowlton <mknowlton89@gmail.com>
2022-04-14 19:59:27 -0400

    Updated config file

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
     env: {
         MONGODB_URI: 'mongodb+srv://mknowlton-admin:Michael2016!@cluster0.hfcgv.mongodb.net/utmGenerator?retryWrites=true&w=majority',
         MONGODB_DB: 'utmGenerator',
+        JWT_SECRET_KEY:'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTYyMzI4NDQ0NSwiaWF0IjoxNjIzMjg0NDQ1fQ.WwHw8M3Q53nuMOdNbOWWVsQJ_U2ml_DzXEwesPhxbu0',
         STRIPE_PUBLIC_KEY: "pk_live_51JbliZA2NnxEX769wLJJG4EafdJuEuUFQ1tC4vd5lQOatJpq0Nrae1uuSXN7YtvmY39qWTNKuYZJ9HQbQUwt8jte00cPyKTaMd",
         STRIPE_PUBLIC_TEST_KEY: "pk_test_51JbliZA2NnxEX7695GhMBYZIVuG1xDYLdrDTkSnwTmDIcbjp1llrwT4DqMK5KhFS5rVReKCM5P2oqq3kIlJStP4o004inxyI2N",
         STRIPE_SECRET_TEST_KEY: "sk_test_51JbliZA2NnxEX769QWNQEcOj9Ivq97Mxt1ZEkvrdaZPCrUChuUsLvnuMV2otI7N3RTaVA9CD5VSlrfHeqxLETxTd00RAgDKbM4"
